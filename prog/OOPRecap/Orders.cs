8da3db23b9a9fc26f713aa349745ceebf2d2f72d
Connor Davis <connor@auto-bot.co.za>
2023-03-09 13:08:09 +0200

    Updates

---
+++ prog/OOPRecap/Orders.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRecap
+{
+    internal class Orders
+    {
+        // Objects
+        private string orderId;
+        private string customerName;
+        private int numberOfBurgers;
+        private int numberOfDrinks;
+
+        public Orders() { }
+
+        public Orders(string orderId, string customerName, int numberOfBurgers, int numberOfDrinks)
+        {
+            this.orderId = orderId;
+            this.customerName = customerName;
+            this.numberOfBurgers = numberOfBurgers;
+            this.numberOfDrinks = numberOfDrinks;
+        }
+
+        public string getOrderId { get { return orderId; } }
+        public string getCustomerName { get { return customerName; } }
+        public int getNumberOfBurgers { get { return numberOfBurgers; } }
+        public int getNumberOfDrinks { get { return numberOfDrinks; } }
+
+        public void setOrderId(string orderId)
+        {
+            this.orderId = orderId;
+        }
+
+        public void setCustomerName(string customerName)
+        {
+            this.customerName = customerName;
+        }
+
+        public void setNumberOfBurgers(int numberOfBurgers)
+        {
+            this.numberOfBurgers = numberOfBurgers;
+        }
+
+        public void setNumberOfDrinks(int numberOfDrinks)
+        {
+            this.numberOfDrinks = numberOfDrinks;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return "[OrderId: " + this.orderId + "; CustomerName: " +this.customerName + "; NumberOfBurgers: " + this.numberOfBurgers + "; NumberOfDrinks: " + this.numberOfDrinks + "]";
+        }
+    }
+}
---
+++ prog/OOPRecap/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.ObjectModel;
+
+namespace OOPRecap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Collection<Orders> ordersCollection = new Collection<Orders>();
+
+            Console.Write("What is the order id> ");
+            string? orderId = Console.ReadLine();
+
+            Console.Write("What is the customer name> ");
+            string? customerName = Console.ReadLine();
+
+            Console.Write("What is the number of burgers> ");
+            int numberOfBurgers = Int32.Parse(Console.ReadLine());
+
+            Console.Write("What is the number of drinks> ");
+            int numberOfDrinks = Int32.Parse(Console.ReadLine());
+
+            ordersCollection.Add(new Orders(orderId, customerName, numberOfBurgers, numberOfDrinks));
+
+            foreach (var order in ordersCollection)
+            {
+                Console.WriteLine(order.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
