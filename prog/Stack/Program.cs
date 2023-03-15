415dd0b4bc20b9c95e1255b4db4c244e39659cee
Connor Davis <cnnrproton@gmail.com>
2023-03-15 15:55:38 +0200

    Updates

---
+++ prog/Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace Stacks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /**
+             * Stack
+             * Create an object of a stack
+             * Imports from the Collections class
+             */
+            Stack stack = new Stack();
+
+            /**
+             * Push -> Inserts an object into the stack
+             * Pop -> Removes & returns an item at the top of the stack
+             * Clear -> Removes all items in the stack
+             * Clone -> Creates a shallow copy of the stack
+             * Contains -> Checks if an item exists in the stack
+             *          -> returns a boolean True/False
+             * Peek -> Returns the top item in the stack
+             * 
+             * -> Last In First Out storage
+             */
+
+            stack.Push("First Item");
+            stack.Push("Second Item");
+            stack.Push(1000);
+            stack.Push(null);
+            stack.Push(3.14159);
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
